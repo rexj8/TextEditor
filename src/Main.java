9695f15533722075b7dc1475eeb29c1982b5d1b8
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-04-12 01:43:06 +0530

    initialising JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Button Example");
+        final JTextField tf = new JTextField();
+
+
+    }
 }
