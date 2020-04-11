5ddc2e01858d1c74121aac3c6dca05688ff18c3d
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-04-12 01:46:27 +0530

    adding Jbutton and textfield

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Button Example");
+
         final JTextField tf = new JTextField();
+        tf.setBounds(50, 50, 150, 20);                // tf coordinates and dimensions
 
+        JButton btn = new JButton("Click Here");
+        btn.setBounds(50, 100, 95, 30);                // btn coordinates and dimensions
 
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.add(btn);
+        frame.add(tf);
+        frame.setSize(400, 400);                            // frame dimensions
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
+
 }
